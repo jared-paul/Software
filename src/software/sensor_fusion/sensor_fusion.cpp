1b80878027c9c15217bafe295c0917d8f39cd25b
jared-paul <jaredpaul9820@gmail.com>
2021-06-23 23:17:02 -0700

    lets see how this works, favouring ball in dribbler over vision

--- src/software/sensor_fusion/sensor_fusion.cpp
+++ src/software/sensor_fusion/sensor_fusion.cpp
@@ -4,24 +4,24 @@
 #include "software/logger/logger.h"
 
 SensorFusion::SensorFusion(std::shared_ptr<const SensorFusionConfig> sensor_fusion_config)
-    : sensor_fusion_config(sensor_fusion_config),
-      field(std::nullopt),
-      ball(std::nullopt),
-      friendly_team(),
-      enemy_team(),
-      game_state(),
-      referee_stage(std::nullopt),
-      ball_filter(sensor_fusion_config->getRollingFrictionAcceleration()->value()),
-      friendly_team_filter(),
-      enemy_team_filter(),
-      team_with_possession(TeamSide::ENEMY),
-      gc_defending_positive_side(false),
-      friendly_goalie_id(0),
-      enemy_goalie_id(0),
-      reset_time_vision_packets_detected(0),
-      last_t_capture(0),
-      // there is no way we have more than 32 robot ids
-      breakbeam_statuses(32, false)
+        : sensor_fusion_config(sensor_fusion_config),
+          field(std::nullopt),
+          ball(std::nullopt),
+          friendly_team(),
+          enemy_team(),
+          game_state(),
+          referee_stage(std::nullopt),
+          ball_filter(sensor_fusion_config->getRollingFrictionAcceleration()->value()),
+          friendly_team_filter(),
+          enemy_team_filter(),
+          team_with_possession(TeamSide::ENEMY),
+          gc_defending_positive_side(false),
+          friendly_goalie_id(0),
+          enemy_goalie_id(0),
+          reset_time_vision_packets_detected(0),
+          last_t_capture(0),
+        // there is no way we have more than 32 robot ids
+          breakbeam_statuses(32, false)
 {
     if (!sensor_fusion_config)
     {
@@ -52,6 +52,7 @@ std::optional<World> SensorFusion::getWorld() const
 
 void SensorFusion::processSensorProto(const SensorProto &sensor_msg)
 {
+    updateWorld(sensor_msg.robot_status_msgs());
     if (sensor_msg.has_ssl_vision_msg())
     {
         updateWorld(sensor_msg.ssl_vision_msg());
@@ -62,7 +63,6 @@ void SensorFusion::processSensorProto(const SensorProto &sensor_msg)
         updateWorld(sensor_msg.ssl_referee_msg());
     }
 
-    updateWorld(sensor_msg.robot_status_msgs());
 
     friendly_team.assignGoalie(friendly_goalie_id);
     enemy_team.assignGoalie(enemy_goalie_id);
@@ -70,30 +70,30 @@ void SensorFusion::processSensorProto(const SensorProto &sensor_msg)
     if (sensor_fusion_config->getOverrideGameControllerFriendlyGoalieId()->value())
     {
         RobotId friendly_goalie_id_override =
-            sensor_fusion_config->getFriendlyGoalieId()->value();
+                sensor_fusion_config->getFriendlyGoalieId()->value();
         friendly_team.assignGoalie(friendly_goalie_id_override);
     }
 
     if (sensor_fusion_config->getOverrideGameControllerEnemyGoalieId()->value())
     {
         RobotId enemy_goalie_id_override =
-            sensor_fusion_config->getEnemyGoalieId()->value();
+                sensor_fusion_config->getEnemyGoalieId()->value();
         enemy_team.assignGoalie(enemy_goalie_id_override);
     }
 
     if (sensor_fusion_config->getOverrideRefereeCommand()->value())
     {
         std::string previous_state_string =
-            sensor_fusion_config->getPreviousRefereeCommand()->value();
+                sensor_fusion_config->getPreviousRefereeCommand()->value();
         std::string current_state_string =
-            sensor_fusion_config->getCurrentRefereeCommand()->value();
+                sensor_fusion_config->getCurrentRefereeCommand()->value();
         try
         {
             RefereeCommand previous_state =
-                fromStringToRefereeCommand(previous_state_string);
+                    fromStringToRefereeCommand(previous_state_string);
             game_state.updateRefereeCommand(previous_state);
             RefereeCommand current_state =
-                fromStringToRefereeCommand(current_state_string);
+                    fromStringToRefereeCommand(current_state_string);
             game_state.updateRefereeCommand(current_state);
         }
         catch (std::invalid_argument e)
@@ -158,12 +158,12 @@ void SensorFusion::updateWorld(const SSLProto::Referee &packet)
     }
 
     const bool override_game_controller_defending_side =
-        sensor_fusion_config->getOverrideGameControllerDefendingSide()->value();
+            sensor_fusion_config->getOverrideGameControllerDefendingSide()->value();
     const bool defending_positive_side =
-        sensor_fusion_config->getDefendingPositiveSide()->value();
+            sensor_fusion_config->getDefendingPositiveSide()->value();
     const bool should_invert_field =
-        (override_game_controller_defending_side && defending_positive_side) ||
-        (!override_game_controller_defending_side && gc_defending_positive_side);
+            (override_game_controller_defending_side && defending_positive_side) ||
+            (!override_game_controller_defending_side && gc_defending_positive_side);
 
     if (game_state.isBallPlacement())
     {
@@ -191,7 +191,7 @@ void SensorFusion::updateWorld(const SSLProto::Referee &packet)
 }
 
 void SensorFusion::updateWorld(
-    const google::protobuf::RepeatedPtrField<TbotsProto::RobotStatus> &robot_status_msgs)
+        const google::protobuf::RepeatedPtrField<TbotsProto::RobotStatus> &robot_status_msgs)
 {
     for (auto &robot_status_msg : robot_status_msgs)
     {
@@ -221,8 +221,8 @@ void SensorFusion::updateWorld(
         friendly_team.setUnavailableRobotCapabilities(robot_id, unavailableCapabilities);
 
         breakbeam_statuses[robot_id] =
-            robot_status_msg.has_break_beam_status() &&
-            robot_status_msg.break_beam_status().ball_in_beam();
+                robot_status_msg.has_break_beam_status() &&
+                robot_status_msg.break_beam_status().ball_in_beam();
     }
 }
 
@@ -231,30 +231,30 @@ void SensorFusion::updateWorld(const SSLProto::SSL_DetectionFrame &ssl_detection
     double min_valid_x = sensor_fusion_config->getMinValidX()->value();
     double max_valid_x = sensor_fusion_config->getMaxValidX()->value();
     bool ignore_invalid_camera_data =
-        sensor_fusion_config->getIgnoreInvalidCameraData()->value();
+            sensor_fusion_config->getIgnoreInvalidCameraData()->value();
 
     // We invert the field side if we explicitly choose to override the values
     // provided by the game controller. The 'defending_positive_side' parameter dictates
     // the side we are defending if we are overriding the value
     const bool override_game_controller_defending_side =
-        sensor_fusion_config->getOverrideGameControllerDefendingSide()->value();
+            sensor_fusion_config->getOverrideGameControllerDefendingSide()->value();
     const bool defending_positive_side =
-        sensor_fusion_config->getDefendingPositiveSide()->value();
+            sensor_fusion_config->getDefendingPositiveSide()->value();
     const bool should_invert_field =
-        (override_game_controller_defending_side && defending_positive_side) ||
-        (!override_game_controller_defending_side && gc_defending_positive_side);
+            (override_game_controller_defending_side && defending_positive_side) ||
+            (!override_game_controller_defending_side && gc_defending_positive_side);
 
     bool friendly_team_is_yellow =
-        sensor_fusion_config->getFriendlyColorYellow()->value();
+            sensor_fusion_config->getFriendlyColorYellow()->value();
 
     auto ball_detections = createBallDetections({ssl_detection_frame}, min_valid_x,
                                                 max_valid_x, ignore_invalid_camera_data);
     auto yellow_team =
-        createTeamDetection({ssl_detection_frame}, TeamColour::YELLOW, min_valid_x,
-                            max_valid_x, ignore_invalid_camera_data);
+            createTeamDetection({ssl_detection_frame}, TeamColour::YELLOW, min_valid_x,
+                                max_valid_x, ignore_invalid_camera_data);
     auto blue_team =
-        createTeamDetection({ssl_detection_frame}, TeamColour::BLUE, min_valid_x,
-                            max_valid_x, ignore_invalid_camera_data);
+            createTeamDetection({ssl_detection_frame}, TeamColour::BLUE, min_valid_x,
+                                max_valid_x, ignore_invalid_camera_data);
 
     if (should_invert_field)
     {
@@ -294,46 +294,51 @@ void SensorFusion::updateWorld(const SSLProto::SSL_DetectionFrame &ssl_detection
     friendly_team.updateRobots(friendly_team_robots);
 
     auto robot_with_ball_in_dribbler_it =
-        std::find_if(friendly_team.getAllRobots().begin(), friendly_team.getAllRobots().end(),
-                     [](const Robot& robot) {return robot.currentState().breakbeamStatus();});
+            std::find_if(friendly_team.getAllRobots().begin(), friendly_team.getAllRobots().end(),
+                         [](const Robot& robot) {return robot.currentState().breakbeamStatus();});
 
 
     std::optional<Ball> new_ball = createBall(ball_detections);
-    if (new_ball)
-    {
-        // If vision detected a new ball, then use that one
-        updateBall(*new_ball);
-    }
-    else if (robot_with_ball_in_dribbler_it != friendly_team.getAllRobots().end())
+    if (robot_with_ball_in_dribbler_it != friendly_team.getAllRobots().end())
     {
         BallDetection dribbler_in_ball_detection = BallDetection{
-            .position =
-            robot_with_ball_in_dribbler_it->position() +
-            Vector::createFromAngle(robot_with_ball_in_dribbler_it->orientation())
-                .normalize(DIST_TO_FRONT_OF_ROBOT_METERS - 0.01),
-            .distance_from_ground = 0,
-            .timestamp  = Timestamp::fromSeconds(ssl_detection_frame.t_capture()),
-            .confidence = 1};
+                .position =
+                robot_with_ball_in_dribbler_it->position() +
+                Vector::createFromAngle(robot_with_ball_in_dribbler_it->orientation())
+                        .normalize(DIST_TO_FRONT_OF_ROBOT_METERS - 0.01),
+                .distance_from_ground = 0,
+                .timestamp  = Timestamp::fromSeconds(ssl_detection_frame.t_capture()),
+                .confidence = 1};
 
-        ball_detections.emplace_back(dribbler_in_ball_detection);
         // try to create a ball again and update it if it exists
-        std::optional<Ball> new_ball_with_dribbler_dets = createBall(ball_detections);
+        std::optional<Ball> new_ball_with_dribbler_dets =
+                createBall({dribbler_in_ball_detection});
+
+
+        std::cout << "OK" << std::endl;
         if (new_ball_with_dribbler_dets)
         {
             updateBall(*new_ball_with_dribbler_dets);
         }
     }
+    else if (new_ball)
+    {
+        std::cout << "AOPEKJRW" << std::endl;
+        // If vision detected a new ball, then use that one
+        updateBall(*new_ball);
+    }
     else if (ball)
     {
         // If we already have a ball from a previous frame, but is occluded this frame
         std::optional<Robot> closest_enemy =
-            enemy_team.getNearestRobot(ball->position());
+                enemy_team.getNearestRobot(ball->position());
 
         // ball is occluded by an enemy, snap the ball to the dribbler of that robot
+        std::cout << "NICE" << std::endl;
         if (closest_enemy.has_value())
         {
             ball = Ball(closest_enemy->position() +
-                            Vector::createFromAngle(closest_enemy->orientation())
+                        Vector::createFromAngle(closest_enemy->orientation())
                                 .normalize(DIST_TO_FRONT_OF_ROBOT_METERS),
                         Vector(0, 0), closest_enemy->timestamp());
         }
@@ -364,7 +369,7 @@ void SensorFusion::updateBall(Ball new_ball)
 }
 
 std::optional<Ball> SensorFusion::createBall(
-    const std::vector<BallDetection> &ball_detections)
+        const std::vector<BallDetection> &ball_detections)
 {
     if (field)
     {
@@ -374,7 +379,7 @@ std::optional<Ball> SensorFusion::createBall(
         }
 
         std::optional<Ball> new_ball =
-            ball_filter.estimateBallState(ball_detections, ball_filter_area);
+                ball_filter.estimateBallState(ball_detections, ball_filter_area);
         return new_ball;
     }
     return std::nullopt;
@@ -383,7 +388,7 @@ std::optional<Ball> SensorFusion::createBall(
 Team SensorFusion::createFriendlyTeam(const std::vector<RobotDetection> &robot_detections)
 {
     Team new_friendly_team =
-        friendly_team_filter.getFilteredData(friendly_team, robot_detections);
+            friendly_team_filter.getFilteredData(friendly_team, robot_detections);
     return new_friendly_team;
 }
 
@@ -396,7 +401,7 @@ Team SensorFusion::createEnemyTeam(const std::vector<RobotDetection> &robot_dete
 RobotDetection SensorFusion::invert(RobotDetection robot_detection) const
 {
     robot_detection.position =
-        Point(-robot_detection.position.x(), -robot_detection.position.y());
+            Point(-robot_detection.position.x(), -robot_detection.position.y());
     robot_detection.orientation = robot_detection.orientation + Angle::half();
     return robot_detection;
 }
@@ -404,7 +409,7 @@ RobotDetection SensorFusion::invert(RobotDetection robot_detection) const
 BallDetection SensorFusion::invert(BallDetection ball_detection) const
 {
     ball_detection.position =
-        Point(-ball_detection.position.x(), -ball_detection.position.y());
+            Point(-ball_detection.position.x(), -ball_detection.position.y());
     return ball_detection;
 }
 
@@ -450,7 +455,7 @@ void SensorFusion::resetWorldComponents()
     game_state    = GameState();
     referee_stage = std::nullopt;
     ball_filter =
-        BallFilter(sensor_fusion_config->getRollingFrictionAcceleration()->value());
+            BallFilter(sensor_fusion_config->getRollingFrictionAcceleration()->value());
     friendly_team_filter = RobotTeamFilter();
     enemy_team_filter    = RobotTeamFilter();
     team_with_possession = TeamSide::ENEMY;
@@ -459,5 +464,5 @@ void SensorFusion::resetWorldComponents()
 void SensorFusion::resetBallFilter()
 {
     ball_filter =
-        BallFilter(sensor_fusion_config->getRollingFrictionAcceleration()->value());
+            BallFilter(sensor_fusion_config->getRollingFrictionAcceleration()->value());
 }
